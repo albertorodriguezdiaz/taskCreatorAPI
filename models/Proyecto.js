78d648ae4319e854353d0fe0c7e08af05bd3ea02
albertorodriguezdiaz <alberto@efectovirtual.com>
2021-11-07 02:23:28 -0500

    Creando el modelo proyecto

---
+++ models/Proyecto.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const  ProyectoSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        require: true,
+        trim:  true
+    },
+    creador: {
+        type: mongoose.Schema.Types.ObjectId, //Tipo Join
+        ref: 'Usuario'
+    },
+    creado: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports = mongoose.model('Proyecto', ProyectoSchema);
