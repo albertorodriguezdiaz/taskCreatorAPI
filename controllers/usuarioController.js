3b2b6e9d6f94d7f1ddc87b448b8797d0e7f42cd0
albertorodriguezdiaz <alberto@efectovirtual.com>
2021-11-07 01:28:23 -0500

    Validaciones a usuarios de la  API

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,5 +1,43 @@
+const Usuario = require('../models/Usuario');
+const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator');
+
+exports.crearUsuario = async (req, res) => {
+
+    // revisar si hay errores
+    const errores = validationResult(req);
+    if (!errores.isEmpty()) {
+        return res.status(400).json({errores: errores.array()});
+    }
+
+    const {email, password} = req.body;
+
+    try {
+        // Revisar que el usuario registrado sea unico
+        let usuario = await Usuario.findOne({email});
+
+        if (usuario) {
+            return res.status(400).json({ msg: 'El usuario ya existe'});
+        }
+
+        // Crea el nuevo usuario
+        usuario = new Usuario(req.body);
 
 
-exports.crearUsuario = (req, res) => {
-    console.log('desde crearUsuario');
+        // Hashear el password
+        // ( salt ) crea un hash diferente para passwords iguales
+        const salt = await bcryptjs.genSalt(10);
+        usuario.password = await bcryptjs.hash(password, salt);
+
+
+        // Guardar usuario
+        await usuario.save();
+
+        // Mensaje de confirmacion
+        res.json({ msg: 'El usuario fue creado exitosamente'});
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('Hubo un error');
+    }
 }
