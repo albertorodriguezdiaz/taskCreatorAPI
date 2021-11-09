02b4f92fa6613286d5d0e500c6452adbaf5ab8ca
albertorodriguezdiaz <alberto@efectovirtual.com>
2021-11-09 00:54:04 -0500

    Usuario autenticado

--- controllers/authController.js
+++ controllers/authController.js
@@ -51,3 +51,16 @@ exports.autenticarUsuario = async (req, res) => {
     }
 
 }
+
+
+
+// Obtener que el usuario esta autenticado
+exports.usuarioAutenticado = async (req, res) =>{
+    try {
+        const usuario = await Usuario.findById(req.usuario.id);
+        res.json({usuario});
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({msg: 'Hubo un error'});
+    }
+}
--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,22 @@ const express = require('express');
 const router = express.Router();
 const { check } = require('express-validator');
 const authController = require('../controllers/authController');
+const auth = require('../middleware/auth');
 
-// Autenticar un usuario
+
 // api/auth
+
+// Obteniendo el usuario autenticado
+router.get('/',
+    auth,
+    [
+        check('email','Agrega un email valido').isEmail(),
+        check('password', 'El password debe ser minimo de 6 caracteres').isLength({min:6})
+    ],
+    authController.usuarioAutenticado
+);
+
+// Inicion sesion
 router.post('/',
     [
         check('email','Agrega un email valido').isEmail(),
